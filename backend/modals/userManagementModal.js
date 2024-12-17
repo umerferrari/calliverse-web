0b0045e695a169b431f84466a300d3708aca1fc4
umerfinjineers <muhammad.omer@finjineers.com>
2024-12-18 02:51:12 +0500

    All code pushed

--- backend/modals/userManagementModal.js
+++ backend/modals/userManagementModal.js
@@ -1,67 +1,205 @@
+// const mongoose = require("mongoose");
+// const bcrypt = require("bcrypt");
+// const jwt = require("jsonwebtoken");
+// const crypto = require("crypto");
+// const UserSchema = new mongoose.Schema(
+//   {
+//     firstName: {
+//       type: String,
+//     },
+//     lastName: {
+//       type: String,
+//     },
+//     // image: {
+//     //   type: String
+//     // },
+//     // Profile image details
+//     profileImage: {
+//       imageUrl: { type: String}, // URL of the image
+//       imageMimeType: { type: String}, // MIME type (e.g., image/jpeg)
+//       imageName: { type: String}, // Original file name
+//       // fieldName: { type: String, required: true } // Field name for identification
+//     },
+//     // Profile image details
+//     // profileImage: {
+//     //   imageUrl: { type: String}, // URL of the image
+//     //   imageMimeType: { type: String}, // MIME type (e.g., image/jpeg)
+//     //   imageName: { type: String}, // Original file name
+//       // fieldName: { type: String, required: true } // Field name for identification
+//     // },
+//     password: {
+//       type: String,
+//       required: true,
+//     },
+//     email: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//     },
+//     // phone: {
+//     //     type: String,
+//     //     required: true,
+//     //     unique: true
+//     // },
+//     bio: {
+//       type: String,
+//     },
+//     websiteLink: {
+//       type: String,
+//     },
+
+//     isProfileCompleted: { type: Boolean, default: false }, 
+  
+
+//     userRole: {
+//       type: String,
+//       enum: ["admin", "user"], // Allowed values
+//       default: "user", // Default value
+//     },
+//     isEmailVerified: {
+//       type: Boolean,
+//       default: false, // Initially set to false
+//     },
+//     // Email verification
+//     emailVerificationCode: String, // Stores the verification code
+//     emailVerificationExpire: Date, // Expiration time for the token
+//     // Reset password
+//     resetPasswordToken: String, // Token to be used for password reset
+//     resetPasswordExpire: Date, // Expiration time for the token
+
+//     isActive: { type: Boolean, default: false },
+//     lastSeen: { type: Date },
+//   },
+//   {
+//     timestamps: true, // Enables createdAt and updatedAt fields automatically
+//   }
+// );
+
+// // Hash password before saving
+// UserSchema.pre("save", async function (next) {
+//   if (this.isModified("password")) {
+//     const salt = await bcrypt.genSalt(10);
+//     this.password = await bcrypt.hash(this.password, salt);
+//   }
+
+//   // Check if profile is completed
+//   if (
+//     this.firstName &&
+//     this.lastName &&
+//     this.email &&
+//     this.password &&
+//     this.isModified("firstName", "lastName", "email", "password")
+//   ) {
+//     this.isProfileCompleted = true;
+//   } else {
+//     this.isProfileCompleted = false;
+//   }
+
+//   next();
+// });
+
+// // //update isProfileCompleted to true when the required fields are filled
+// // UserSchema.pre("findOneAndUpdate", async function (next) {
+// //   const update = this.getUpdate();
+
+// //   // Check if necessary fields are being updated
+// //   if (update.firstName && update.lastName && update.email && update.password) {
+// //     update.isProfileCompleted = true;
+// //   } else {
+// //     update.isProfileCompleted = false;
+// //   }
+
+// //   this.setUpdate(update);
+// //   next();
+// // });
+
+
+
+
+
+// UserSchema.methods.createJWT = function () {
+//   return jwt.sign(
+//     { userId: this._id, name: this.firstName, role: this.userRole },
+//     process.env.JWT_SECRET,
+//     {
+//       expiresIn: process.env.JWT_LIFETIME,
+//     }
+//   );
+// };
+
+// UserSchema.methods.comparePassword = async function (canditatePassword) {
+//   const isMatch = await bcrypt.compare(canditatePassword, this.password);
+//   return isMatch;
+// };
+
+// // Generate and hash password reset token
+// UserSchema.methods.getResetPasswordToken = function () {
+//   const resetToken = crypto.randomBytes(20).toString("hex");
+
+//   // Hash token and set to resetPasswordToken field
+//   this.resetPasswordToken = crypto
+//     .createHash("sha256")
+//     .update(resetToken)
+//     .digest("hex");
+
+//   // Set expire time (e.g., 10 minutes)
+//   this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+
+//   return resetToken;
+// };
+
+// UserSchema.methods.generateEmailVerificationCode = function () {
+//   const verificationCode = Math.floor(
+//     100000 + Math.random() * 900000
+//   ).toString(); // Generate a 6-digit code
+//   this.emailVerificationCode = crypto
+//     .createHash("sha256")
+//     .update(verificationCode)
+//     .digest("hex"); // Hash the code
+//   this.emailVerificationExpire = Date.now() + 2 * 60 * 1000; // Code valid for 10 minutes
+//   return verificationCode; // Return unhashed code for sending via email
+// };
+
+// module.exports = mongoose.model("User", UserSchema);
+
+
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
+
 const UserSchema = new mongoose.Schema(
   {
-    firstName: {
-      type: String,
-    },
-    lastName: {
-      type: String,
-    },
-    // Profile image details
+    firstName: { type: String },
+    lastName: { type: String },
     profileImage: {
-      imageUrl: { type: String}, // URL of the image
-      imageMimeType: { type: String}, // MIME type (e.g., image/jpeg)
-      imageName: { type: String}, // Original file name
-      // fieldName: { type: String, required: true } // Field name for identification
+      imageUrl: { type: String }, // URL of the image
+      imageMimeType: { type: String },
+      imageName: { type: String },
     },
-    password: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    // phone: {
-    //     type: String,
-    //     required: true,
-    //     unique: true
-    // },
-    bio: {
-      type: String,
-    },
-    websiteLink: {
-      type: String,
-    },
-
-    isProfileCompleted: { type: Boolean, default: false }, 
-  
-
-    userRole: {
-      type: String,
-      enum: ["admin", "user"], // Allowed values
-      default: "user", // Default value
-    },
-    isEmailVerified: {
-      type: Boolean,
-      default: false, // Initially set to false
-    },
-    // Email verification
-    emailVerificationCode: String, // Stores the verification code
-    emailVerificationExpire: Date, // Expiration time for the token
-    // Reset password
-    resetPasswordToken: String, // Token to be used for password reset
-    resetPasswordExpire: Date, // Expiration time for the token
-
+    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    bio: { type: String },
+    websiteLink: { type: String },
+    isProfileCompleted: { type: Boolean, default: false },
+    userRole: { type: String, enum: ["admin", "user"], default: "user" },
+    isEmailVerified: { type: Boolean, default: false },
+    emailVerificationCode: String,
+    emailVerificationExpire: Date,
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
     isActive: { type: Boolean, default: false },
     lastSeen: { type: Date },
   },
   {
-    timestamps: true, // Enables createdAt and updatedAt fields automatically
+    timestamps: true,
+    toJSON: { virtuals: true, transform: function (doc, ret) { 
+      // Modify the output object
+      ret.profileImage = ret.profileImage?.imageUrl || null; 
+      delete ret.password; // Exclude password for safety
+      return ret; 
+    }},
+    toObject: { virtuals: true }, // Apply to object conversions
   }
 );
 
@@ -71,83 +209,37 @@ UserSchema.pre("save", async function (next) {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
   }
-
-  // Check if profile is completed
-  if (
-    this.firstName &&
-    this.lastName &&
-    this.email &&
-    this.password &&
-    this.isModified("firstName", "lastName", "email", "password")
-  ) {
-    this.isProfileCompleted = true;
-  } else {
-    this.isProfileCompleted = false;
-  }
-
   next();
 });
 
-// //update isProfileCompleted to true when the required fields are filled
-// UserSchema.pre("findOneAndUpdate", async function (next) {
-//   const update = this.getUpdate();
-
-//   // Check if necessary fields are being updated
-//   if (update.firstName && update.lastName && update.email && update.password) {
-//     update.isProfileCompleted = true;
-//   } else {
-//     update.isProfileCompleted = false;
-//   }
-
-//   this.setUpdate(update);
-//   next();
-// });
-
-
-
-
-
+// JWT creation
 UserSchema.methods.createJWT = function () {
   return jwt.sign(
     { userId: this._id, name: this.firstName, role: this.userRole },
     process.env.JWT_SECRET,
-    {
-      expiresIn: process.env.JWT_LIFETIME,
-    }
+    { expiresIn: process.env.JWT_LIFETIME }
   );
 };
 
+// Compare password
 UserSchema.methods.comparePassword = async function (canditatePassword) {
-  const isMatch = await bcrypt.compare(canditatePassword, this.password);
-  return isMatch;
+  return await bcrypt.compare(canditatePassword, this.password);
 };
 
-// Generate and hash password reset token
+// Generate reset password token
 UserSchema.methods.getResetPasswordToken = function () {
   const resetToken = crypto.randomBytes(20).toString("hex");
-
-  // Hash token and set to resetPasswordToken field
-  this.resetPasswordToken = crypto
-    .createHash("sha256")
-    .update(resetToken)
-    .digest("hex");
-
-  // Set expire time (e.g., 10 minutes)
+  this.resetPasswordToken = crypto.createHash("sha256").update(resetToken).digest("hex");
   this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
-
   return resetToken;
 };
 
+// Generate email verification code
 UserSchema.methods.generateEmailVerificationCode = function () {
-  const verificationCode = Math.floor(
-    100000 + Math.random() * 900000
-  ).toString(); // Generate a 6-digit code
-  this.emailVerificationCode = crypto
-    .createHash("sha256")
-    .update(verificationCode)
-    .digest("hex"); // Hash the code
-  this.emailVerificationExpire = Date.now() + 2 * 60 * 1000; // Code valid for 10 minutes
-  return verificationCode; // Return unhashed code for sending via email
+  const verificationCode = Math.floor(100000 + Math.random() * 900000).toString();
+  this.emailVerificationCode = crypto.createHash("sha256").update(verificationCode).digest("hex");
+  this.emailVerificationExpire = Date.now() + 2 * 60 * 1000;
+  return verificationCode;
 };
 
 module.exports = mongoose.model("User", UserSchema);
--- backend/sockets/socketHandlers/userEvents.js
+++ backend/sockets/socketHandlers/userEvents.js
@@ -1,34 +1,92 @@
+// const Message = require('../../modals/messageManagementModel');
+// const getUndeliveredMessages = require('../../controller/messageManagementController')
+
+// module.exports = (io, socket, onlineUsers) => {
+//   // User comes online
+//   socket.on("userOnline", async (userId) => {
+//     onlineUsers.set(userId, socket.id);
+
+//     // Fetch undelivered messages
+//     const undeliveredMessages = await Message.find({ receiverId: userId, delivered: false });
+//     console.log("underliever messages",undeliveredMessages)
+
+//     const recipientSocketId = onlineUsers.get(userId); 
+//     if (undeliveredMessages.length) {
+//       undeliveredMessages.forEach((message) => {
+//         io.to(recipientSocketId).emit("receiveMessage", message, (ack)=>{
+//           if (ack?.success) {
+//             console.log(
+//               `Message ${message._id} successfully delivered to client.`
+//             );
+//           } else {
+//             console.error(`Message ${message._id} delivery failed.`);
+//           }
+//         });
+//         message.delivered = true;
+//         message.save();
+//       });
+//     }
+
+//     console.log(`${userId} is online.`);
+//     io.emit("onlineUsers", Array.from(onlineUsers.keys()));
+//   });
+
+//   // User disconnects
+//   socket.on("disconnect", () => {
+//     let disconnectedUserId;
+//     onlineUsers.forEach((value, key) => {
+//       if (value === socket.id) {
+//         disconnectedUserId = key;
+//         onlineUsers.delete(key);
+//       }
+//     });
+
+//     if (disconnectedUserId) {
+//       console.log(`${disconnectedUserId} went offline.`);
+//       io.emit("onlineUsers", Array.from(onlineUsers.keys()));
+//     }
+//   });
+// };
+
 const Message = require('../../modals/messageManagementModel');
-const getUndeliveredMessages = require('../../controller/messageManagementController')
+const getUndeliveredMessages = require('../../controller/messageManagementController');
 
 module.exports = (io, socket, onlineUsers) => {
   // User comes online
   socket.on("userOnline", async (userId) => {
-    onlineUsers.set(userId, socket.id);
+    try {
+      onlineUsers.set(userId, socket.id);
 
-    // Fetch undelivered messages
-    const undeliveredMessages = await Message.find({ receiverId: userId, delivered: false });
-    console.log("underliever messages",undeliveredMessages)
-
-    const recipientSocketId = onlineUsers.get(userId); 
-    if (undeliveredMessages.length) {
-      undeliveredMessages.forEach((message) => {
-        io.to(recipientSocketId).emit("receiveMessage", message, (ack)=>{
-          if (ack?.success) {
-            console.log(
-              `Message ${message._id} successfully delivered to client.`
-            );
-          } else {
-            console.error(`Message ${message._id} delivery failed.`);
-          }
-        });
-        message.delivered = true;
-        message.save();
+      // Fetch undelivered messages
+      const undeliveredMessages = await Message.find({
+        receiverId: userId,
+        delivered: false,
       });
-    }
 
-    console.log(`${userId} is online.`);
-    io.emit("onlineUsers", Array.from(onlineUsers.keys()));
+      console.log("Undelivered messages:", undeliveredMessages);
+
+      const recipientSocketId = onlineUsers.get(userId);
+      if (undeliveredMessages.length) {
+        undeliveredMessages.forEach((message) => {
+          io.to(recipientSocketId).emit("receiveMessage", message, (ack) => {
+            if (ack?.success) {
+              console.log(
+                `Message ${message._id} successfully delivered to client.`
+              );
+            } else {
+              console.error(`Message ${message._id} delivery failed.`);
+            }
+          });
+          message.delivered = true;
+          message.save();
+        });
+      }
+
+      console.log(`${userId} is online.`);
+      io.emit("onlineUsers", Array.from(onlineUsers.keys()));
+    } catch (error) {
+      console.error("Error in userOnline event:", error.message);
+    }
   });
 
   // User disconnects
